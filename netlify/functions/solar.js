65cd9385dea92f28dedc304e2cbfb8aec0affea1
stefanbrunner08 <stefanbrunner08@gmail.com>
2026-08-21 09:29:19 +0200

    Add files via upload

--- netlify/functions/solar.js
+++ netlify/functions/solar.js
@@ -1,5 +1,5 @@
 exports.handler = async () => {
-  const KIOSK_URL = "https://uni001eu5.fusionsolar.huawei.com/rest/pvms/web/kiosk/v1/station-kiosk-file?kk=Fr8l9ItFy5uoGJPO8homHdo5p4GHiqOh";
+  const KIOSK_URL = "https://uni001eu5.fusionsolar.huawei.com/pvmswebsite/nologin/assets/build/cloud.html#/kiosk?kk=ie2viBuxotNw5fLrkQJF7JyJ7H8jGbvI";
   try {
     const response = await fetch(KIOSK_URL,{headers:{"User-Agent":"Mozilla/5.0"}});
     const text = await response.text();
