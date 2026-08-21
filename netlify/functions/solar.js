3fc9916cfe09849bd8ee950bc1c978fb7edd22c5
stefanbrunner08 <stefanbrunner08@gmail.com>
2026-08-21 09:59:32 +0200

    Add files via upload

--- netlify/functions/solar.js
+++ netlify/functions/solar.js
@@ -1,10 +1,67 @@
 exports.handler = async () => {
-  const KIOSK_URL = "https://uni001eu5.fusionsolar.huawei.com/pvmswebsite/nologin/assets/build/cloud.html#/kiosk?kk=ie2viBuxotNw5fLrkQJF7JyJ7H8jGbvI";
-  try {
-    const response = await fetch(KIOSK_URL,{headers:{"User-Agent":"Mozilla/5.0"}});
-    const text = await response.text();
-    return {statusCode:200,headers:{"Access-Control-Allow-Origin":"*","Content-Type":"application/json"},body:text};
-  } catch(err){
-    return {statusCode:500,headers:{"Content-Type":"application/json"},body:JSON.stringify({success:false,error:err.message})};
-  }
+2
+const API_URL =
+3
+"https://uni001eu5.fusionsolar.huawei.com/rest/pvms/web/kiosk/v1/station-kiosk-file?kk=ie2viBuxotNw5fLrkQJF7JyJ7H8jGbvI";
+4
+ 
+5
+try {
+6
+const response = await fetch(API_URL, {
+7
+headers: {
+8
+"User-Agent": "Mozilla/5.0"
+9
+}
+10
+});
+11
+ 
+12
+const text = await response.text();
+13
+ 
+14
+return {
+15
+statusCode: 200,
+16
+headers: {
+17
+"Access-Control-Allow-Origin": "*",
+18
+"Content-Type": "application/json"
+19
+},
+20
+body: text
+21
+};
+22
+} catch (err) {
+23
+return {
+24
+statusCode: 500,
+25
+headers: {
+26
+"Content-Type": "application/json"
+27
+},
+28
+body: JSON.stringify({
+29
+success: false,
+30
+error: err.message
+31
+})
+32
+};
+33
+}
+34
 };
